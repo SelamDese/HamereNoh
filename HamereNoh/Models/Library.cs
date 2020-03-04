aba21731ba20bf41448aa88ccb34a9def9ac7895
millket <milliyon.ketera@gmail.com>
2020-03-04 09:19:00 -0600

    entities

--- HamereNoh/Models/Library.cs
+++ HamereNoh/Models/Library.cs
@@ -11,5 +11,10 @@ namespace HamereNoh.Models
         public string ItemName { get; set; }
         public string ItemType { get; set; }
         public int BatchId { get; set; }
+        public int CourceId { get; set; }
+        public int StudentId { get; set; }
+        public int TeacherId { get; set; }
+        public Student Student { get; set; }
+        public Teacher Teacher { get; set; }
     }
 }
